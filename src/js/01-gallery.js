07f32d42be3762a628c12b7cda5b94f0372000ef
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-21 15:47:35 +0200

    Added script to gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryCardsSet = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item" style="border-radius: 4px; background: transparent; box-shadow: none;">
+  <a class="gallery__item" href="${original}" style= "box-shadow: none;" style="border-radius: 4px;">
+  <img class="gallery__image" src="${preview}" alt="${description}" style="border: 1px solid transparent;
+  border-radius: 4px;
+  padding: 5px; background: rgba(255, 255, 255, 0.3); box-shadow: 0px 1px 3px 0px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 2px 1px -1px rgb(0 0 0 / 12%);"/>
+</a>
+</div>`;
+    })
+    .join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardsSet);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
